be6032aae7f80418eeee55c731dd2321f6a6cbf7
Andrewij21 <andrewijaya988@gmail.com>
2023-08-13 18:51:24 +0700

    refactor: add global reset mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import CssBaseline from "@mui/material/CssBaseline";
 import Layout from "./components/layout/Layout";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Layout>
+    <>
+      <CssBaseline />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Layout>
+    </>
   );
 }
 
