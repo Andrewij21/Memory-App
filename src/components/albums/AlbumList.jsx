c6f545bdaef40a27fffe47d6f6f3758d8008188d
Andrewij21 <andrewijaya988@gmail.com>
2023-08-14 08:12:30 +0700

    refactor: renaming components

--- src/components/albums/AlbumList.jsx
+++ src/components/albums/AlbumList.jsx
@@ -1,5 +1,5 @@
 import { Grid } from "@mui/material";
-import MeetupItem from "./MeetupItem";
+import MeetupItem from "./AlbumItem";
 import PropTypes from "prop-types";
 
 const MeetupList = ({ photos }) => {
