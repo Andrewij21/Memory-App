44a37c28a34cd2e4cc3decf3c68aa9bbce639682
Andrewij21 <andrewijaya988@gmail.com>
2023-08-14 08:56:34 +0700

    feat: fetch data from firebase

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,28 @@
 import { Container, Typography } from "@mui/material";
-import MeetupList from "../components/meetups/MeetupList";
-import data from "../data.json";
+import MeetupList from "../components/albums/AlbumList";
+// import data from "../data.json";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  let [albums, setAlbums] = useState([]);
+  useEffect(() => {
+    fetch("https://react-album-1a909-default-rtdb.firebaseio.com/albums.json")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        let loadedAlbums = [];
+        for (const key in data) {
+          loadedAlbums.push({ id: key, ...data[key] });
+        }
+        // console.log(data);
+        // console.log(loadedAlbums);
+        setAlbums(loadedAlbums);
+      });
+  }, []);
   return (
     <Container>
-      {data.map((album) => {
+      {albums.map((album) => {
         return (
           <div key={album.id}>
             <Typography
