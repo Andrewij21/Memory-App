467adacf6c052c54a3ffbca3a1322dd62504106c
Andrewij21 <andrewijaya988@gmail.com>
2023-08-15 07:18:11 +0700

    refactor: add dayjs formater and change some style

--- src/components/albums/AlbumItem.jsx
+++ src/components/albums/AlbumItem.jsx
@@ -8,9 +8,10 @@ import {
   IconButton,
   Typography,
 } from "@mui/material";
+import dayjs from "dayjs";
 import PropTypes from "prop-types";
 
-const MeetupItem = ({ photo }) => {
+const AlbumItem = ({ photo }) => {
   return (
     <Grid item xs={4}>
       <Card sx={{ maxWidth: 345, minHeight: 380 }} raised>
@@ -21,15 +22,26 @@ const MeetupItem = ({ photo }) => {
           image={photo.image}
         />
         <CardContent>
-          <Box display="flex" justifyContent="space-between">
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            sx={{ minHeight: 70 }}
+          >
             <Typography gutterBottom variant="h6" component="h3">
               {photo.name}
             </Typography>
-            <Box>
-              <IconButton aria-label="delete" color="error" size="small">
+
+            <Box sx={{ display: "flex" }}>
+              <IconButton
+                aria-label="delete"
+                color="error"
+                size="small"
+                disableRipple
+              >
                 <Delete fontSize="small" />
               </IconButton>
-              <IconButton aria-label="more" size="small">
+              <IconButton aria-label="more" size="small" disableRipple>
                 <MoreVert fontSize="small" />
               </IconButton>
             </Box>
@@ -37,7 +49,7 @@ const MeetupItem = ({ photo }) => {
 
           <Box>
             <Typography variant="caption" color="text.secondary" component="p">
-              {photo.date}
+              {dayjs(photo.date).format("DD/MM/YYYY")}
             </Typography>
             <Typography variant="body2" component="p">
               {photo.description}
@@ -49,7 +61,7 @@ const MeetupItem = ({ photo }) => {
   );
 };
 
-MeetupItem.propTypes = {
+AlbumItem.propTypes = {
   photo: PropTypes.object,
 };
-export default MeetupItem;
+export default AlbumItem;
