762ba9647307bba4c4b05f6e5561b277c767cfd8
Andrewij21 <andrewijaya988@gmail.com>
2023-08-29 10:25:23 +0700

    feat: can see full image

---
+++ src/components/albums/FullImage.jsx
@@ -0,0 +1,29 @@
+import { Card, CardMedia, Paper } from "@mui/material";
+
+const FullImage = (image) => {
+  return (
+    <Paper
+      sx={{
+        display: "flex",
+        maxWidth: "100vh",
+        maxHeight: "80vh",
+        alignSelf: "center",
+        backgroundColor: "transparent",
+      }}
+      elevation={0}
+    >
+      <CardMedia
+        component="img"
+        sx={{
+          width: "100%",
+          objectFit: "contain",
+        }}
+        alt={image.name}
+        // image={image.image}
+        image={image.image}
+      />
+    </Paper>
+  );
+};
+
+export default FullImage;
