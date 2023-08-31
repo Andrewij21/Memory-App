ab0a6a2cae71ed832a80cb81879523b63cb5721e
Andrewij21 <andrewijaya988@gmail.com>
2023-08-31 17:59:24 +0700

    feat: add login and register route

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import CssBaseline from "@mui/material/CssBaseline";
 import Layout from "./components/layout/Layout";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import NewPhoto from "./pages/NewPhoto";
+import Registration from "./pages/auth/Registration";
+import Login from "./pages/auth/Login";
 
 function App() {
   return (
     <>
-      <CssBaseline />
-      <Layout>
-        <Routes>
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="registration" element={<Registration />} />
+        <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/new-photo" element={<NewPhoto />} />
-        </Routes>
-      </Layout>
+          <Route path="new-photo" element={<NewPhoto />} />
+        </Route>
+      </Routes>
     </>
   );
 }
