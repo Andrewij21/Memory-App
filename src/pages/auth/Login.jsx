c6e1cc997db0b727a57adde7b7420c2d8d02078a
Andrewij21 <andrewijaya988@gmail.com>
2023-08-31 19:26:55 +0700

    feat: add axios and set auth

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,11 +1,32 @@
-import { useEffect } from "react";
 import AuthForm from "../../components/auth/AuthForm";
 import axios from "../../api/axios";
+import useAuth from "../../hooks/useAuth";
+
 const Login = () => {
-  useEffect(() => {
-    axios.post();
-  }, []);
-  return <AuthForm type="login" />;
+  const { setAuth } = useAuth();
+  const handleLogin = async ({ email, password }) => {
+    try {
+      const response = await axios.post(
+        "/auth",
+        {
+          email,
+          password,
+        },
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+      console.log({ response });
+      const roles = response.data.data.roles;
+      const accessToken = response.data.data.accessToken;
+      setAuth({ email, password, roles, accessToken });
+    } catch (error) {
+      console.log({ error: error.toString() });
+      alert(error.response.data.message);
+    }
+  };
+  return <AuthForm type="login" handler={handleLogin} title="sign in" />;
 };
 
 export default Login;
