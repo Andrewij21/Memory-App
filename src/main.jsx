e5dfb6fbaba186094165dee0b39795d5cf9bf8cd
Andrewij21 <andrewijaya988@gmail.com>
2023-08-31 13:43:38 +0700

    style: add Global reset

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import CssBaseline from "@mui/material/CssBaseline";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <React.StrictMode>
+      <CssBaseline />
       <App />
     </React.StrictMode>
   </BrowserRouter>
