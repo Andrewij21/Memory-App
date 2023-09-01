b1150a384a5dbd5afd8764e6bbfceb8aa699003f
Andrewij21 <andrewijaya988@gmail.com>
2023-09-01 10:55:46 +0700

    refactor: improve style

--- src/pages/auth/Unauthorized.jsx
+++ src/pages/auth/Unauthorized.jsx
@@ -1,5 +1,23 @@
+import { Box, Button, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
 const Unauthorized = () => {
-  return <h1>Unauthorized</h1>;
+  const navigate = useNavigate();
+  const backHandler = () => {
+    navigate(-1);
+  };
+
+  return (
+    <Box component="section">
+      <Typography variant="h3" component="h1">
+        Unauthorized
+      </Typography>
+      <Typography variant="p" component="p" gutterBottom>
+        You do not have access to this page
+      </Typography>
+      <Button onClick={backHandler}>Go back</Button>
+    </Box>
+  );
 };
 
 export default Unauthorized;
