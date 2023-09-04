764eb434ea9842b56d3e62cd13e7fdccf07ffc2d
Andrewij21 <andrewijaya988@gmail.com>
2023-09-04 21:35:40 +0700

    feat: create private conn hook

---
+++ src/hooks/useAxiosPrivate.jsx
@@ -0,0 +1,42 @@
+import useAuth from "./useAuth";
+import { axiosPrivate } from "../api/axios";
+import useRefreshToken from "./useRefreshToken";
+import { useEffect } from "react";
+
+const useAxiosPrivate = () => {
+  const { auth } = useAuth();
+  const refresh = useRefreshToken();
+
+  useEffect(() => {
+    const request = axiosPrivate.interceptors.request.use(
+      (response) => {
+        if (!response.headers["Authorization"]) {
+          response.headers["Authorization"] = `Bearer ${auth.accessToken}`;
+        }
+        return response;
+      },
+      (err) => Promise.reject(err)
+    );
+    const response = axiosPrivate.interceptors.response.use(
+      (response) => response,
+      async (err) => {
+        const prevRequest = err.config;
+        if (err.response.status === 403 && !prevRequest.sent) {
+          prevRequest.sent = true;
+          const newAccessToken = await refresh();
+
+          prevRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
+          return axiosPrivate(prevRequest);
+        }
+        return Promise.reject(err);
+      }
+    );
+    return () => {
+      axiosPrivate.interceptors.request.use(request);
+      axiosPrivate.interceptors.response.use(response);
+    };
+  }, [auth, refresh]);
+  return axiosPrivate;
+};
+
+export default useAxiosPrivate;
