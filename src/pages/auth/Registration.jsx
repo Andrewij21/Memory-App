714ba1c7669dc6e576b938cb4a9e77cb8c93968b
Andrewij21 <andrewijaya988@gmail.com>
2023-09-06 15:16:42 +0700

    feat: add register logic

--- src/pages/auth/Registration.jsx
+++ src/pages/auth/Registration.jsx
@@ -1,7 +1,22 @@
 import AuthForm from "../../components/auth/AuthForm";
-
+import axios from "../../api/axios";
+import { useNavigate } from "react-router-dom";
 const Registration = () => {
-  return <AuthForm type="register" title="sign up" />;
+  const navigate = useNavigate();
+
+  const handleRegister = async (payload) => {
+    try {
+      await axios.post("/register", payload, {
+        headers: { "Content-Type": "application/json" },
+        withCredentials: true,
+      });
+      navigate("/");
+    } catch (error) {
+      console.log({ error });
+      return error.response.status;
+    }
+  };
+  return <AuthForm type="register" title="sign up" handler={handleRegister} />;
 };
 
 export default Registration;
