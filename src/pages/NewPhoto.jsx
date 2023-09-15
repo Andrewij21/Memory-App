6953d52e95ba69d8770bf93219eb4072f526c1c2
Andrewij21 <andrewijaya988@gmail.com>
2023-09-15 13:08:26 +0700

    refactor: remove formData object

--- src/pages/NewPhoto.jsx
+++ src/pages/NewPhoto.jsx
@@ -8,14 +8,7 @@ export default function NewPhoto() {
 
   const addPhotoHandler = async (photo) => {
     try {
-      const formData = new FormData();
-      formData.append("name", photo.name);
-      formData.append("image", photo.image);
-      formData.append("description", photo.description);
-      formData.append("date", photo.date);
-      formData.append("user", photo.user);
-
-      await axiosPrivate.post(`/album`, formData, {
+      await axiosPrivate.post(`/album`, photo, {
         headers: { "Content-Type": "multipart/form-data" },
       });
       console.log("data di tambah");
