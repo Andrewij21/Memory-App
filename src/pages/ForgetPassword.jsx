7742362de01a3643003d532e868e55448c9235c3
Andrewij21 <andrewijaya988@gmail.com>
2023-09-27 12:49:32 +0700

    feat: create forget password component

---
+++ src/pages/ForgetPassword.jsx
@@ -0,0 +1,68 @@
+import {
+  Avatar,
+  Box,
+  Button,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { useRef } from "react";
+
+const ForgetPassword = () => {
+  const email = useRef();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Box
+      sx={{
+        marginTop: 8,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Forget Password
+      </Typography>
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        sx={{
+          mt: 3,
+          width: "70vh",
+        }}
+      >
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={12}>
+            <TextField
+              inputRef={email}
+              name="email"
+              required
+              fullWidth
+              label="Email"
+              autoFocus
+              type="email"
+            />
+          </Grid>
+        </Grid>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2, textTransform: "uppercase" }}
+        >
+          Submit
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default ForgetPassword;
