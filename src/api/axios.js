ef4c702ce6dd718d0904a463c2efa22120cbc016
andrewijaya <andrewijaya988@gmail.com>
2023-12-22 10:56:14 +0700

    update: get url from env

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = import.meta.env.VITE_API;
+// const BASE_URL = "http://localhost:3000";
 
 export default axios.create({
   baseURL: BASE_URL,
